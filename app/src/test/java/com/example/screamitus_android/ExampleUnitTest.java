132eb1ad15ec8c054080959f2ac6fb6d4d360c9e
zangrazyjatt <eramriksidhu@gmail.com>
2019-09-19 12:29:05 -0400

    R2-Green upto day seven Infected Instructors increments by 5, later on the increment is 8 infected instructors per day.

--- app/src/test/java/com/example/screamitus_android/ExampleUnitTest.java
+++ app/src/test/java/com/example/screamitus_android/ExampleUnitTest.java
@@ -27,11 +27,45 @@ Infection infection;
         int days = 0;
         assertEquals("-1",  infection.ZeroDays(-1));
     }
-
     @Test
-    public void DysUpToSeven() {
+    public void Day1() {
         int days = 1;
         assertEquals("numberOfInfectedInstructors = 5",  infection.ZeroDays(days));
+    }
+    @Test
+    public void Day2() {
+        int days = 2;
+        assertEquals("numberOfInfectedInstructors = 10",  infection.ZeroDays(days));
+    }
+    @Test
+    public void Day3() {
+        int days = 3;
+        assertEquals("numberOfInfectedInstructors = 15",  infection.ZeroDays(days));
+    }
+    @Test
+    public void Day4() {
+        int days = 4;
+        assertEquals("numberOfInfectedInstructors = 20",  infection.ZeroDays(days));
+    }
+    @Test
+    public void Day5() {
+        int days = 5;
+        assertEquals("numberOfInfectedInstructors = 25",  infection.ZeroDays(days));
+    }
+    @Test
+    public void Day6() {
+        int days = 6;
+        assertEquals("numberOfInfectedInstructors = 30",  infection.ZeroDays(days));
+    }
+    @Test
+    public void Day7() {
+        int days = 7;
+        assertEquals("numberOfInfectedInstructors = 35",  infection.ZeroDays(days));
+    }
+    @Test
+    public void Day8() {
+        int days = 8;
+        assertEquals("numberOfInfectedInstructors = 43",  infection.ZeroDays(days));
     }
 
 }
