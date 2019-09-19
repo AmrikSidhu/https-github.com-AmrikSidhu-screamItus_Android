a3b351b0087cdacd66322f94d88fb6d2b47bbf39
zangrazyjatt <eramriksidhu@gmail.com>
2019-09-19 11:54:55 -0400

    R1-Red if days equals to zero must return "-1"

--- app/src/main/java/com/example/screamitus_android/Infection.java
+++ app/src/main/java/com/example/screamitus_android/Infection.java
@@ -1,7 +1,35 @@
 package com.example.screamitus_android;
 
+
 public class Infection {
-    //data
+    private int numberOfInfectedInstructors = 5;
+    private int numberOfInfectedInstructorsEight = 8;
+    // creating empty constructor
+    public  Infection()
+    {
+
+    }
+    public String ZeroDays(int days) {
+        if (days <= 0) {
+            return "-1";
+        }
+        else if (days <=7){
+
+            for (int i = 1; i <= 7; i++) {
+                numberOfInfectedInstructors = numberOfInfectedInstructors * 2;
+                System.out.println(numberOfInfectedInstructors);
+
+            }
+
+        }
+else  {
+                numberOfInfectedInstructorsEight = numberOfInfectedInstructorsEight * 2;
+                System.out.println(numberOfInfectedInstructorsEight);
+
+            }
+        return String.valueOf(numberOfInfectedInstructors);
+        }
 
 
-}
+
+    }
--- app/src/test/java/com/example/screamitus_android/ExampleUnitTest.java
+++ app/src/test/java/com/example/screamitus_android/ExampleUnitTest.java
@@ -1,5 +1,6 @@
 package com.example.screamitus_android;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +11,21 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+Infection infection;
+
+    @Before
+    public void setUp() throws Exception {
+        infection = new Infection();
+    }
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void ZeroDays() {
+        int days = 0;
+        assertEquals("Days",  infection.ZeroDays(-1));
+    }
+
 }
